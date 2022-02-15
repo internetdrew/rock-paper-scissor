60bc35c43e97ed4e79e553f6a0c75b09bd4fc65e
Andrew Rowley <andrew@internetdrew.com>
2022-02-15 11:28:11 -0500

    Fix logic for game play on click

--- app2.js
+++ app2.js
@@ -2,8 +2,48 @@ const rock = document.querySelector('#rock');
 const paper = document.querySelector('#paper');
 const scissor = document.querySelector('#scissor');
 
-function playRound(){}
+function capitalize(str) {
+  let newStr = str[0].toUpperCase() + str.slice(1).toLowerCase();
+  return newStr;
+}
 
-rock.addEventListener('click', playRound(e){
+function getComputerPlay() {
+  let options = ['rock', 'paper', 'scissor'];
+  let random = Math.floor(Math.random() * options.length);
+  computerSelection = options[random];
+  return computerSelection;
+}
 
-});
+function determineRoundWinner(computerSelection, userSelection) {
+  let roundWinner;
+  if (userSelection === computerSelection) {
+    console.log(`Tie! You both played ${userSelection}.`);
+    roundWinner = null;
+    return;
+  } else if (
+    (userSelection === 'rock' && computerSelection === 'scissor') ||
+    (userSelection === 'paper' && computerSelection === 'rock') ||
+    (userSelection === 'scissor' && computerSelection === 'paper')
+  ) {
+    userSelection = capitalize(userSelection);
+    console.log(`You won! ${userSelection} beats ${computerSelection}!`);
+    roundWinner = 'user';
+  } else {
+    console.log(
+      `You lose this one. The computer played ${computerSelection}, which beats your ${userSelection}.`
+    );
+    roundWinner = 'computer';
+  }
+  return roundWinner;
+}
+
+function playRound(event) {
+  let userSelection = event.target.value;
+  let computerSelection = getComputerPlay();
+  let roundWinner = determineRoundWinner(computerSelection, userSelection);
+}
+
+// Event Listeners
+rock.addEventListener('click', playRound);
+paper.addEventListener('click', playRound);
+scissor.addEventListener('click', playRound);
