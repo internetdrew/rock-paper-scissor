cdaa2ca617971213263942fc491f9d472386f857
Andrew Rowley <andrew@internetdrew.com>
2022-02-15 11:05:22 -0500

    Add event listeners for each button

---
+++ app2.js
@@ -0,0 +1,9 @@
+const rock = document.querySelector('#rock');
+const paper = document.querySelector('#paper');
+const scissor = document.querySelector('#scissor');
+
+function playRound(){}
+
+rock.addEventListener('click', playRound(e){
+
+});
