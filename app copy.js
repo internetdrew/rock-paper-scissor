be1bf6d69770f1027b203f8fbb63ee3bf0caefa7
Andrew Rowley <andrew@internetdrew.com>
2022-02-15 08:48:58 -0500

    Eliminate play button for streamlined play

--- app copy.js
+++ app copy.js
@@ -1,15 +1,6 @@
-const playButton = document.getElementById('play-button');
 const game = document.getElementsByClassName('game');
 const rock = document.getElementById('rock');
 const paper = document.getElementById('paper');
 const scissor = document.getElementById('scissor');
 
-const options = {
-  name: 'rock',
-};
-
 // Event Listeners
-playButton.addEventListener('click', () => {
-  playButton.classList.add('hidden');
-  game.classList.remove('hidden');
-});
