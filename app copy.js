c4e6a58c08018a78cf0deebab921dc93e534371f
Andrew Rowley <andrew@internetdrew.com>
2022-02-15 10:58:30 -0500

    Reinstate blank slate JS file

--- app copy.js
+++
@@ -1,6 +0,0 @@
-const game = document.getElementsByClassName('game');
-const rock = document.getElementById('rock');
-const paper = document.getElementById('paper');
-const scissor = document.getElementById('scissor');
-
-// Event Listeners
